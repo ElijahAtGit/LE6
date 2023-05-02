b799b710766893b2d4e595394d5717b1358710fc
Elijah Pyon <76900263+SirLalaPyon@users.noreply.github.com>
2023-05-02 15:00:33 +0800

    Added backend

---
+++ TAM-ODLE5/backend/Tam-odLE4/LastNameBlogDB/Stored Procedures/spUsers_Register.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Register]
+	@userName NVARCHAR(16),
+	@firstName NVARCHAR(50),
+	@lastName NVARCHAR(50),
+	@password NVARCHAR(16)
+
+AS
+begin
+	set nocount on;
+
+	INSERT INTO dbo.Users
+	(UserName, FirstName, LastName, Password)
+	VALUES (@userName, @firstName, @lastName, @password)
+
+end
